ddbd68c7b7296bee3c499c430a33584527ed0f8f
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 12:22:24 +0100

    Refactor substrings method

--- script.rb
+++ script.rb
@@ -1,17 +1,15 @@
 require 'pry-byebug'
 
 def substrings(string, dict_arr)
-    string_arr = string.split(" ")
-    new_arr = []
-    string_arr.each do |item|
+    matches_arr = []
+    string.split(" ").each do |item|
         filter = dict_arr.find_all { |i| item.downcase.include?(i) }
-        p filter
-        new_arr += filter
+        matches_arr += filter
     end
-    p new_arr
-    hash = {}
-    new_arr.each { |i| hash[i] ? hash[i] += 1 : hash[i] = 1 }
-    hash
+    matches_arr.reduce({}) do |hash, i|
+        hash[i] ? hash[i] += 1 : hash[i] = 1
+        hash
+    end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below"]
