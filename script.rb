e09f310bacde8d4d26822202a13e90b19d1a8c63
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 09:58:26 +0100

    Add substrings method
    - Generates a hash with name and quantities of matches between single word and dictionary array

--- script.rb
+++ script.rb
@@ -1,1 +1,12 @@
+require 'pry-byebug'
 
+def substrings(string, dict_arr)
+    filter = dict_arr.find_all { |i| string.include?(i) }
+    hash = {}
+    filter.each { |i| hash[i] ? hash[i] += 1 : hash[i] = 1 }
+    hash
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
