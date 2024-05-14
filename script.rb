99a48d096eb45a25c92061565eb0f430cddbceea
Toby Parsons <toby.d.parsons@gmail.com>
2024-05-14 11:32:09 +0100

    Add multiple word compatability
    substrings method now supports a string that includes multiple words. Each word will be checked against the dictionary

--- script.rb
+++ script.rb
@@ -1,12 +1,19 @@
 require 'pry-byebug'
 
 def substrings(string, dict_arr)
-    filter = dict_arr.find_all { |i| string.include?(i) }
+    string_arr = string.split(" ")
+    new_arr = []
+    string_arr.each do |item|
+        filter = dict_arr.find_all { |i| item.downcase.include?(i) }
+        p filter
+        new_arr += filter
+    end
+    p new_arr
     hash = {}
-    filter.each { |i| hash[i] ? hash[i] += 1 : hash[i] = 1 }
+    new_arr.each { |i| hash[i] ? hash[i] += 1 : hash[i] = 1 }
     hash
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "below"]
 
-substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
